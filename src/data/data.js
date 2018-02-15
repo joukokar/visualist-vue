7955946b58be5ea0b093449811d61a76ed01bf35
Jouko Karvonen <joukokar@gmail.com>
2018-02-15 11:52:36 +0900

    Preserve column types of data

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ import keys from 'lodash/keys';
 import map from 'lodash/map';
 import uniq from 'lodash/uniq';
 import filter from 'lodash/filter';
-import { isDate } from 'lodash';
+import { isDate, isNumber, isBoolean, isString } from 'lodash';
 import * as d3 from 'd3';
 
 export default class VstData {
@@ -16,6 +16,43 @@ export default class VstData {
       d.$$index = i;
       i += 1;
     });
+  }
+
+  static getColumnTypes(data) {
+    const allKeys = VstData.getAllKeys(data);
+    const columnTypes = {};
+    each(allKeys, (k) => {
+      let type;
+      for (let j = 0; j < data.length; j += 1) {
+        let suspectedType;
+        const d = data[j][k];
+        if (isDate(d)) {
+          suspectedType = 'Date';
+        } else if (isNumber(d)) {
+          suspectedType = 'Number';
+        } else if (isBoolean(d)) {
+          suspectedType = 'Boolean';
+        } else if (isString(d)) {
+          suspectedType = 'String';
+        } else {
+          // eslint-disable-next-line no-console
+          console.warn(`Unknown type in ${k}: ${d}`);
+          suspectedType = 'mixed';
+        }
+
+        if (type && type !== suspectedType) {
+          // eslint-disable-next-line no-console
+          console.warn(`Multiple types mixed in ${k}: ${type}, ${suspectedType}`);
+          suspectedType = 'mixed';
+          break;
+        }
+        if (!type) {
+          type = suspectedType;
+        }
+      }
+      columnTypes[k] = type;
+    });
+    return columnTypes;
   }
 
   static figureMinStepOfData(data, prop = 'x') {
@@ -41,25 +78,39 @@ export default class VstData {
     return d;
   }
 
-  static fillNA(data) {
-    const xValues = map(data, d => VstData.getValue(d.x));
+  static fillNA(data, prop = 'x') {
+    const xValues = map(data, d => VstData.getValue(d[prop]));
     const xExtent = d3.extent(xValues);
     const newData = [];
 
     const allKeys = VstData.getAllKeys(data);
 
-    const step = VstData.figureMinStepOfData(data, 'x');
+    const step = VstData.figureMinStepOfData(data, prop);
+
+    const columnTypes = this.getColumnTypes(data);
 
     for (let i = xExtent[0]; i < xExtent[1]; i += step) {
-      const existingObject = find(this.data, d => VstData.getValue(d.x) === i);
+      const existingObject = find(this.data, d => VstData.getValue(d[prop]) === i);
       let newObject = existingObject;
       if (!newObject) {
-        newObject = { x: i };
+        if (columnTypes[prop] === 'Date') {
+          newObject = { x: new Date(i) };
+        } else if (columnTypes[prop] === 'String') {
+          newObject = { x: i.toString() };
+        } else {
+          newObject = { x: i };
+        }
       }
 
       each(allKeys, (key) => {
         if (!newObject[key]) {
-          newObject[key] = 0;
+          if (columnTypes[key] === 'Date') {
+            newObject[key] = new Date();
+          } else if (columnTypes[key] === 'Number') {
+            newObject[key] = 0;
+          } else if (columnTypes[key] === 'String') {
+            newObject[key] = '';
+          }
         }
       });
       newData.push(newObject);
